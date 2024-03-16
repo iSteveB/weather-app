7a79f9510f383bd6c06bfe8974388be7cb8ba9b8
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2024-03-16 15:39:07 +0100

    fix typo

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,67 +1,73 @@
-import React from 'react'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { Feather } from '@expo/vector-icons'
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Feather } from '@expo/vector-icons';
 
-import CurrentWeather from '../screens/CurrentWeather'
-import UpComingWeather from '../screens/UpcomingWeather'
-import City from '../screens/City'
+import CurrentWeather from '../screens/CurrentWeather';
+import UpComingWeather from '../screens/UpComingWeather';
+import City from '../screens/City';
 
-
-const Tab = createBottomTabNavigator()
+const Tab = createBottomTabNavigator();
 
 const Tabs = ({ weather }) => {
-    return (
-        <Tab.Navigator
-        screenOptions={{
-          tabBarActiveTintColor: '#fffffe',
-          tabBarInactiveTintColor: 'gray',
-          tabBarStyle: {
-            backgroundColor: '#004643',
-          },
-          headerStyle: {
-            backgroundColor: '#004643',
-          },
-   
-          headerTitleStyle: {
-            fontWeight: 'bold',
-            fontSize: 25,
-            color: '#fffffe',
-          },
-        }}
-      >
-        <Tab.Screen
-          name="Current"
-          options={{
-            tabBarIcon: ({ focused }) => (
-              <Feather
-                name="droplet"
-                size={25}
-                color={focused ? '#fffffe' : 'gray'}
-              />
-            )
-          }}
-        >{()=> <CurrentWeather weatherData={weather.list[0]} />}</Tab.Screen>
-        <Tab.Screen name="Upcoming" options={{
-            tabBarIcon: ({ focused }) => (
-              <Feather
-                name="clock"
-                size={25}
-                color={focused ? '#fffffe' : 'gray'}
-              />
-            )
-          }}>{() => <UpComingWeather weatherData={weather.list} />} </Tab.Screen>
+	return (
+		<Tab.Navigator
+			screenOptions={{
+				tabBarActiveTintColor: '#fffffe',
+				tabBarInactiveTintColor: 'gray',
+				tabBarStyle: {
+					backgroundColor: '#004643',
+				},
+				headerStyle: {
+					backgroundColor: '#004643',
+				},
 
-        <Tab.Screen name="City" component={City} options={{
-            tabBarIcon: ({ focused }) => (
-              <Feather
-                name="home"
-                size={25}
-                color={focused ? '#fffffe' : 'gray'}
-              />
-            )
-          }}/>
-      </Tab.Navigator>
-    );
+				headerTitleStyle: {
+					fontWeight: 'bold',
+					fontSize: 25,
+					color: '#fffffe',
+				},
+			}}>
+			<Tab.Screen
+				name='Current'
+				options={{
+					tabBarIcon: ({ focused }) => (
+						<Feather
+							name='droplet'
+							size={25}
+							color={focused ? '#fffffe' : 'gray'}
+						/>
+					),
+				}}>
+				{() => <CurrentWeather weatherData={weather.list[0]} />}
+			</Tab.Screen>
+			<Tab.Screen
+				name={'Upcoming'}
+				options={{
+					tabBarIcon: ({ focused }) => (
+						<Feather
+							name='clock'
+							size={25}
+							color={focused ? '#fffffe' : 'gray'}
+						/>
+					)
+				}}>
+				{() => <UpComingWeather weatherData={weather.list} />}</Tab.Screen>
+
+			<Tab.Screen
+				name='City'
+				component={City}
+				options={{
+					tabBarIcon: ({ focused }) => (
+						<Feather
+							name='home'
+							size={25}
+							color={focused ? '#fffffe' : 'gray'}
+						/>
+					),
+				}}
+			/>
+		</Tab.Navigator>
+	);
 };
 
 export default Tabs;
--- src/screens/UpcomingWeather.js
+++ src/screens/UpcomingWeather.js
@@ -1,88 +1,82 @@
-import React from 'react'
-import {
-  StyleSheet,
-  FlatList,
-  View,
-  ImageBackground
-} from 'react-native'
+import React from 'react';
+import { StyleSheet, FlatList, View, ImageBackground } from 'react-native';
 
 import ListItem from '../components/ListItem';
 
 const DATA = [
-  {
-    dt_txt: ' 2024-03-14 10:00:00',
-    main: {
-      temp_max: 12.7,
-      temp_min: 8.3
-    },
-    weather: [
-      {
-        main: 'Clear'
-      }
-    ]
-  },
-  {
-    dt_txt: ' 2024-03-16 09:00:00',
-    main: {
-      temp_max: 16.5,
-      temp_min: 9.2
-    },
-    weather: [
-      {
-        main: 'Cloudy'
-      }
-    ]
-  },
-  {
-    dt_txt: ' 2024-03-17 12:10:00',
-    main: {
-      temp_max: 13.5,
-      temp_min: 8.8
-    },
-    weather: [
-      {
-        main: 'Sunny'
-      }
-    ]
-  }
-]
+	{
+		dt_txt: ' 2024-03-14 10:00:00',
+		main: {
+			temp_max: 12.7,
+			temp_min: 8.3,
+		},
+		weather: [
+			{
+				main: 'Clear',
+			},
+		],
+	},
+	{
+		dt_txt: ' 2024-03-16 09:00:00',
+		main: {
+			temp_max: 16.5,
+			temp_min: 9.2,
+		},
+		weather: [
+			{
+				main: 'Cloudy',
+			},
+		],
+	},
+	{
+		dt_txt: ' 2024-03-17 12:10:00',
+		main: {
+			temp_max: 13.5,
+			temp_min: 8.8,
+		},
+		weather: [
+			{
+				main: 'Sunny',
+			},
+		],
+	},
+];
 
-const UpcomingWeather = () => {
-  const renderItem = ({ item }) => (
-    <ListItem
-      dt_txt={item.dt_txt}
-      min={item.main.temp_min}
-      max={item.main.temp_max}
-      condition={item.weather[0].main}
-    />
-  )
+const UpComingWeather = ({ weatherData }) => {
+	const renderItem = ({ item }) => (
+		<ListItem
+			dt_txt={item.dt_txt}
+			min={item.main.temp_min}
+			max={item.main.temp_max}
+			condition={item.weather[0].main}
+		/>
+	);
 
-  const { container, image } = styles;
+	const { container, image } = styles;
 
-  return (
-      <View style={container}>
-        <ImageBackground
-          source={require('../../assets/upcoming-weather.jpg')}
-          style={image}
-        >
-          <FlatList
-            data={DATA}
-            renderItem={renderItem}
-            keyExtractor={(item) => item.dt_txt}
-          />
-        </ImageBackground>
-      </View>
-  )
-}
+	return (
+		<View style={container}>
+			<ImageBackground
+				source={require('../../assets/upcoming-weather.jpg')}
+				style={image}>
+				<FlatList
+					data={DATA}
+					renderItem={renderItem}
+					keyExtractor={(item) => item.dt_txt}
+				/>
+			</ImageBackground>
+		</View>
+	);
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  image: {
-    flex: 1,
-    resizeMode: 'cover',
-    justifyContent: 'center'
-  }
-})
-export default UpcomingWeather
+	container: {
+		flex: 1,
+	},
+	image: {
+		flex: 1,
+		resizeMode: 'cover',
+		justifyContent: 'center',
+	},
+});
+export default UpComingWeather;
